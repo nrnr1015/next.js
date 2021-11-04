98225b6d084bbe5762370992e4b89d61f7b177e5
nrnr1015 <llr2916@naver.com>
2021-11-04 13:58:20 +0900

    error edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 
 import "../styles/globals.css";
 import TransitionLayout from "../component/Layout";
-import Navigation from "../component/Navigation.js";
+// import Navigation from "../component/Navigation.js";
 
 // import Home from "./index";
 // import Swipers from "./Swipers";
