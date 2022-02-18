e872342b88f7a3f2ef2f8cdea0cd7787ed5c766f
nrnr1015 <llr2916@naver.com>
2022-02-18 18:27:31 +0900

    워크 영역 map 함수 선언

---
+++ data/workData.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default [
+	{
+		id : 1,
+		link : "https://nrnr1015.notion.site/6e46beb922614a41937a038918cc403e",
+		img : "/thms/sellerApp.jpg",
+		year : 2022,
+		title : "미트박스 판매자 MobileWeb",
+		skill : "#html5&amp;#scss&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 2,
+		link : "https://nrnr1015.notion.site/MOBILE-WEB-dcc8f6a9f1374fb39bf3e8b8d7f21408",
+		img : "/thms/meatboxThm.jpg",
+		year : "유지보수&amp;리뉴얼",
+		title : "MEATBOX MOBILE Web",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 3,
+		link : "https://nrnr1015.github.io/coxi-html-mo-master",
+		img : "/thms/coxiThm.jpg",
+		year : 2019,
+		title : "COXI MOBILE Web",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 4,
+		link : "https://nrnr1015.github.io/iblAdmin/",
+		img : "/thms/iblThm.jpg",
+		year : 2019,
+		title : "IBL ADMIN Web",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 5,
+		link : "http://www.unioncement.com/index.php",
+		img : "/thms/union.png",
+		year : 2016,
+		title : "Union Responsive Web",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 6,
+		link : "https://www.yg1.kr/kor/main.asp",
+		img : "/thms/yg1.png",
+		year : 2016,
+		title : "YG-1 Responsive Web",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 7,
+		link : "http://www.graphenesq.co.kr/",
+		img : "/thms/graphene.png",
+		year : 2016,
+		title : "Graphenesq WEB",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+	{
+		id : 8,
+		link : "http://2016pams.pams.or.kr/mobile/main.asp",
+		img : "/thms/pams2016.png",
+		year : 2016,
+		title : "PAMS 2016 MOBILE WEB",
+		skill : "#html5&amp;#css&amp;#jquery&amp;#javascript"
+	},
+]
--- pages/Work.js
+++ pages/Work.js
@@ -3,11 +3,10 @@ import Link from 'next/link'
 import Fade from "react-reveal/Fade";
 import Typrtwriter from "../component/Typrtwriter";
 import Intro from "../component/Intro";
-import Footer from "../component/Footer";
-
+import workData from "../data/workData";
 
 export default function Work() {
-
+	const [workDataList, setWorkDataList] = useState(workData);
   return (
 	<div className="content-container">
 		<div className="header-wrapper">
@@ -17,105 +16,27 @@ export default function Work() {
 		
 		<div className="work-container">
 			<div className="grid-wrapper">
-
-				<Fade bottom>
-					<Link href="https://nrnr1015.notion.site/6e46beb922614a41937a038918cc403e">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/sellerApp.jpg" />
-								<div className="project-content">
-									<span>2022</span>
-									<h2>미트박스 판매자 Web/App</h2>
-									<span>#html5&amp;#scss&amp;#jquery&amp; #javascript</span>
-								</div>
+				<Fade bottom>	
+				{ 
+					workDataList.map(function( workDataList, i ){ 
+						return ( 
+							<div className="work_list" key={workDataList.id}>
+								<Link href={workDataList.link}>
+									<a target="_blank" className="project-item item1">
+										<div className="image-wrapper">
+											<img src={workDataList.img} />
+											<div className="project-content">
+												<span>{workDataList.year}</span>
+												<h2>{workDataList.title} </h2>
+												<span>{workDataList.skill} </span>
+											</div>
+										</div>
+									</a>
+								</Link>
 							</div>
-						</a>
-					</Link>
-					<Link href="https://nrnr1015.notion.site/MOBILE-WEB-dcc8f6a9f1374fb39bf3e8b8d7f21408">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/meatboxThm.jpg" />
-								<div className="project-content">
-									<span>유지보수&amp;리뉴얼</span>
-									<h2>MEATBOX MOBILE Web</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					
-					<Link href="https://nrnr1015.github.io/coxi-html-mo-master">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/coxiThm.jpg" />
-								<div className="project-content">
-									<span>2019</span>
-									<h2>COXI MOBILE Web</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					<Link href="https://nrnr1015.github.io/iblAdmin/">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/iblThm.jpg" />
-								<div className="project-content">
-									<span>2019</span>
-									<h2>IBL ADMIN Web</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					<Link href="http://www.unioncement.com/index.php">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/union.png" />
-								<div className="project-content">
-									<span>2016</span>
-									<h2>Union Responsive Web</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					<Link href="https://www.yg1.kr/kor/main.asp">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/yg1.png" />
-								<div className="project-content">
-									<span>2016</span>
-									<h2>YG-1 Responsive Web</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					<Link href="http://www.graphenesq.co.kr/">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/graphene.png" />
-								<div className="project-content">
-									<span>2016</span>
-									<h2>Graphenesq WEB</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
-					<Link href="http://2016pams.pams.or.kr/mobile/main.asp">
-						<a target="_blank" className="project-item item1">
-							<div className="image-wrapper">
-								<img src="/thms/pams2016.png" />
-								<div className="project-content">
-									<span>2016</span>
-									<h2>PAMS 2016 MOBILE</h2>
-									<span>#html5&amp;#css&amp;#jquery&amp; #javascript</span>
-								</div>
-							</div>
-						</a>
-					</Link>
+						) 
+					}) 
+				}
 				</Fade>	
 			</div>
 		</div>
@@ -123,4 +44,3 @@ export default function Work() {
     
   )
 }
-
